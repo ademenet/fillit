0f1be6a14bc18942a22f2fcad2f24f45642c8ba1
Alain DEMENET <ademenet@e2r9p10.42.fr>
2015-12-10 17:28:05 +0100

    Calcule la racine carree d'un nombre. Prend un int et retourne un long (pour le moment) qui correspond a la partie entiere de la reponse. + a ajouter a la libft.

---
+++ ft_sqrt.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademenet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/10 14:58:28 by ademenet          #+#    #+#             */
+/*   Updated: 2015/12/10 16:29:55 by ademenet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+long	ft_sqrt(int nb)
+{
+	long	n;
+	long	p;
+	long	low;
+	long	high;
+
+	if (2 > nb)
+		return (nb);
+	low = 0;
+	high = nb;
+	while (high > low + 1)
+	{
+		n = (high + low) / 2;
+		p = n * n;
+		if (nb < p)
+			high = n;
+		else if (nb > p)
+			low = n;
+		else
+			break;
+	}
+	return (nb == p ? n : low);
+}
