3c73ad62e0f0fdbceaec5a828b5ce83b933ad279
Alain DEMENET <ademenet@e2r8p11.42.fr>
2015-12-11 18:55:58 +0100

    Ajout de tests sur strsetnew et createmap.

--- test/test_fillit.c
+++ test/test_fillit.c
@@ -13,11 +13,13 @@
 #define check_str_eq(std, own) if(!strcmp(std, own)) \
 { printf("\033[32mo\033[32m"); } else { printf("\033[31mx\033[31m"); }
 
-#define check(cond) if(cond) { printf("\033[32mo\033[32m"); }\
-	else { printf("\033[31mx\033[31m"); }
+#define check(cond) if(cond) { printf("\033[32mo\033[0m"); }\
+	else { printf("\033[31mx\033[0m"); }
 
 long	ft_sqrt(int nb);
 int		ft_bsq(int area);
+char	*ft_strsetnew(char c, int size);
+char	**ft_createmap(int bsq);
 
 void	test_ft_sqrt()
 {
@@ -37,9 +39,29 @@ void	test_ft_bsq()
 	check(ft_bsq(8) == 6);
 }
 
+void	test_ft_strset()
+{
+	check(strcmp(ft_strsetnew(7, 'B'), "BBBBBB") == 0);
+}
+
+void	test_ft_createmap()
+{
+	char	**s;
+	int	i = 0;
+	s = ft_createmap(4);
+	while (s[i])
+	{
+	printf("%lu", sizeof(s));
+		printf("%s\n", s[i]);
+		i++;
+	}
+}
+
 int		main(void)
 {
 	MY_TEST("ft_sqrt", test_ft_sqrt);
 	MY_TEST("ft_bsq", test_ft_bsq);
+	MY_TEST("ft_strsetnew", test_ft_strset);
+	MY_TEST("ft_createmap", test_ft_createmap);
 	return (0);
 }
