ae900b6a30822f8fc6137852b5c7176c1940c81e
Alain DEMENET <ademenet@e2r8p11.42.fr>
2015-12-11 18:55:06 +0100

    Genere une map de cote voulu et avec des '.'. A debugger.

--- src/ft_createmap.c
+++ src/ft_createmap.c
@@ -6,11 +6,14 @@
 /*   By: ademenet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/11 10:37:57 by ademenet          #+#    #+#             */
-/*   Updated: 2015/12/11 12:14:15 by ademenet         ###   ########.fr       */
+/*   Updated: 2015/12/11 18:47:02 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strset(const char *str, )
+#include <stdlib.h>
+
+char	*ft_strnew(size_t size);
+char	*ft_strsetnew(char c, int size);
 
 char	**ft_createmap(int bsq)
 {
@@ -19,12 +22,16 @@ char	**ft_createmap(int bsq)
 	int		i;
 
 	map_y = (char**)malloc(bsq * sizeof(char*));
-	while (map_y[i])
+	if (map_y)
 	{
-		map_x = ft_strnew(bsq);
-		ft_strset(map_x, '.');
-		map_y[i] = map_x;
-		i++;
+		i = bsq;
+		while (--i)
+		{
+			map_x = ft_strsetnew('.', bsq);
+			*map_y = map_x;
+			map_y++;
+		}
+		return (map_y);
 	}
-	return (map_y);
+	return (0);
 }
