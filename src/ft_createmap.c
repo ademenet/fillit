2d1ea18be4e568878753b395f999823ba7384136
Alain DEMENET <ademenet@e2r9p8.42.fr>
2015-12-14 10:53:22 +0100

    Genere un tableau de chaines de caracteres initiliases avec des '.'. A ete teste. Ameliorations de code et refactorisation.

--- src/ft_createmap.c
+++ src/ft_createmap.c
@@ -6,13 +6,13 @@
 /*   By: ademenet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/11 10:37:57 by ademenet          #+#    #+#             */
-/*   Updated: 2015/12/11 18:47:02 by ademenet         ###   ########.fr       */
+/*   Updated: 2015/12/14 10:46:23 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <string.h>
 
-char	*ft_strnew(size_t size);
 char	*ft_strsetnew(char c, int size);
 
 char	**ft_createmap(int bsq)
@@ -21,17 +21,15 @@ char	**ft_createmap(int bsq)
 	char	*map_x;
 	int		i;
 
-	map_y = (char**)malloc(bsq * sizeof(char*));
-	if (map_y)
+	map_y = (char**)malloc((bsq + 1) * sizeof(char*));
+	if (!map_y)
+		return (NULL);
+	i = bsq;
+	map_y[i] = NULL;
+	while (--i >= 0)
 	{
-		i = bsq;
-		while (--i)
-		{
-			map_x = ft_strsetnew('.', bsq);
-			*map_y = map_x;
-			map_y++;
-		}
-		return (map_y);
+		map_x = ft_strsetnew('.', bsq);
+		map_y[i] = map_x;
 	}
-	return (0);
+	return (map_y);
 }
