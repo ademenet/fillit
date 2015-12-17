84c0c3bac49c39db60c76de2a97f81cb494e7769
Amine DERRAGUI <aderragu@e2r9p10.42.fr>
2015-12-17 22:51:08 +0100

    file that contains the functions which initialise each point of a tetriminos

--- points_assignment.c
+++ points_assignment.c
@@ -12,55 +12,53 @@
 
 #include "fillit.h"
 
-void	ft_p1(char *buf, t_tetri *tetri, int x_ref, int y_ref)
+int	ft_p1(char *buf, t_tetri *tetri, int x_ref, int y_ref)
 {
 	int	cur;
 	int	shrp_cnt;
 
 	if (tetri == NULL)
-		return;
+		return (0);
 	cur = -1;
 	shrp_cnt = 0;
-	while(shrp_cnt < 2)
-		if(buf[++cur] == '#')
+	while (shrp_cnt != 2)
+		if (buf[++cur] == '#')
 			shrp_cnt++;
-	tetri->p1[0] = cur / 5;
-	tetri->p1[1] = cur % 5;
-	if (ft_norme_vector(tetri->p1[1], tetri->p1[0], x_ref, y_ref) == 0)
-		tetri = NULL;
+	tetri->p1[0] = cur / 5 - y_ref;
+	tetri->p1[1] = cur % 5 - x_ref;
+	return (ft_norme_vector(cur, buf));
 }
 
-void	ft_p2(char *buf, t_tetri *tetri, int x_ref, int y_ref)
+int	ft_p2(char *buf, t_tetri *tetri, int x_ref, int y_ref)
 {
 	int	cur;
 	int	shrp_cnt;
 
 	if (tetri == NULL)
-		return;
+		return (0);
 	cur = -1;
 	shrp_cnt = 0;
-	while(shrp_cnt < 3)
-		if(buf[++cur] == '#')
+	while (shrp_cnt != 3)
+		if (buf[++cur] == '#')
 			shrp_cnt++;
-	tetri->p2[0] = cur / 5;
-	tetri->p2[1] = cur % 5;
-	if (ft_norme_vector(tetri->p2[1], tetri->p2[0], x_ref, y_ref) == 0)
-		tetri = NULL;
+	tetri->p2[0] = cur / 5 - y_ref;
+	tetri->p2[1] = cur % 5 - x_ref;
+	return (ft_norme_vector(cur, buf));
 }
-void	ft_p3(char *buf, t_tetri *tetri, int x_ref, int y_ref)
+
+int	ft_p3(char *buf, t_tetri *tetri, int x_ref, int y_ref)
 {
 	int	cur;
 	int	shrp_cnt;
 
 	if (tetri == NULL)
-		return;
+		return (0);
 	cur = -1;
 	shrp_cnt = 0;
-	while(shrp_cnt < 4)
-		if(buf[++cur] == '#')
+	while (shrp_cnt != 4)
+		if (buf[++cur] == '#')
 			shrp_cnt++;
-	tetri->p3[0] = cur / 5;
-	tetri->p3[1] = cur % 5;
-	if (ft_norme_vector(tetri->p3[1], tetri->p3[0], x_ref, y_ref) == 0)
-		tetri = NULL;
+	tetri->p3[0] = cur / 5 - y_ref;
+	tetri->p3[1] = cur % 5 - x_ref;
+	return (ft_norme_vector(cur, buf));
 }
