57d73f70d0025440cd2133cf4c2776386524d1c5
Amine DERRAGUI <aderragu@e2r9p10.42.fr>
2015-12-17 22:49:55 +0100

    file that contains the checking functions

--- check_file.c
+++ check_file.c
@@ -9,27 +9,38 @@
 /*   Updated: 2015/12/11 09:48:00 by aderragu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-//gcc -Wall -Werror -Wextra *.c ./test/main_test.c
-//./a.out ./test/file_test
+
 #include "fillit.h"
-#include <string.h>
 
-int ft_norme_vector(int x, int y, int x_ref, int y_ref)
+int			ft_norme_vector(int x, char *buf)
 {
-	int xdiff;
-	int ydiff;
-
-	xdiff = x - x_ref;
-	ydiff = y - y_ref;
-	if()
-		return (1);
-	else
-		return (0);
+	if (x > 5 && x < 15)
+	{
+		if (buf[x + 1] == '#' || buf[x + 5] == '#' || buf[x - 1] == '#' ||
+		buf[x - 5] == '#')
+			return (1);
+	}
+	if (x == 0)
+	{
+		if (buf[x + 1] == '#' || buf[x + 5] == '#')
+			return (1);
+	}
+	if (x > 0 && x <= 5)
+	{
+		if (buf[x + 1] == '#' || buf[x + 5] == '#' || buf[x - 1] == '#')
+			return (1);
+	}
+	if (x > 15 && x < 21)
+	{
+		if (buf[x + 1] == '#' || buf[x - 1] == '#' || buf[x - 5] == '#')
+			return (1);
+	}
+	return (0);
 }
 
-t_tetri		*ft_pattern_check(char *buf, t_tetri *tetri) //check the pattern contained in a block 
+t_tetri		*ft_pattern_check(char *buf, t_tetri *tetri)
 {
-	int 	cur;
+	int		cur;
 	int		shrp_cnt;
 	int		x_ref;
 	int		y_ref;
@@ -40,19 +51,17 @@ t_tetri		*ft_pattern_check(char *buf, t_tetri *tetri) //check the pattern contai
 		cur++;
 	x_ref = cur % 5;
 	y_ref = cur / 5;
-	ft_p1(buf, tetri, x_ref,y_ref);
-	ft_p2(buf, tetri, tetri->p1[1], tetri->p1[0]);
-	ft_p3(buf, tetri, tetri->p2[1], tetri->p2[0]);
-	if (tetri != NULL)
-		return (tetri);
-	else
-		return (NULL);
+	if (ft_p1(buf, tetri, x_ref, y_ref))
+		if (ft_p2(buf, tetri, x_ref, y_ref))
+			if (ft_p3(buf, tetri, x_ref, y_ref))
+				return (tetri);
+	return (NULL);
 }
 
-t_tetri		*ft_block_check(char *buf, t_tetri *tetri) //check a block and the associated pattern and return a valid tetriminos 
+t_tetri		*ft_block_check(char *buf, t_tetri *tetri)
 {
 	int		cur;
-	int 	dot_cnt;
+	int		dot_cnt;
 	int		shrp_cnt;
 	int		nwl_cnt;
 
@@ -60,26 +69,24 @@ t_tetri		*ft_block_check(char *buf, t_tetri *tetri) //check a block and the asso
 	dot_cnt = 0;
 	shrp_cnt = 0;
 	nwl_cnt = 0;
-	while(buf[++cur] && (buf[cur] == '.' || buf[cur] == '#' ||
+	while (buf[++cur] && (buf[cur] == '.' || buf[cur] == '#' ||
 		buf[cur] == '\n'))
 	{
 		if (buf[cur] == '.')
 			dot_cnt++;
-		if (buf[cur] == '\n')
+		else if (buf[cur] == '\n')
 			nwl_cnt++;
-		if (buf[cur] == '#')
+		else if (buf[cur] == '#')
 			shrp_cnt++;
 	}
-	printf(" . = %d, # = %d, n = %d\n",dot_cnt, shrp_cnt, nwl_cnt);
 	if (dot_cnt != 12 || shrp_cnt != 4 || nwl_cnt != 5)
 		return (NULL);
 	else
 		return (ft_pattern_check(buf, tetri));
 }
 
-t_tetri		*ft_global_check(char *file_name) //check the file and return an array with all the tetriminos avaiable
+t_tetri		*ft_global_check(char *file_name, int *pcs)
 {
-
 	char	*buf;
 	t_tetri	*tetri_array;
 	int		fd;
@@ -91,9 +98,10 @@ t_tetri		*ft_global_check(char *file_name) //check the file and return an array 
 	buf = malloc(sizeof(char));
 	fd = open(file_name, O_RDONLY, 0555);
 	while (read(fd, buf, BUFF))
-		{
-			if(ft_block_check(buf, &(tetri_array[++cnt])) == NULL)
-				return (NULL);
-		}
+	{
+		if (ft_block_check(buf, &(tetri_array[++cnt])) == NULL)
+			return (NULL);
+		*pcs = *pcs + 1;
+	}
 	return (tetri_array);
 }
