f1204fc154e0995befd2b6c6bddd44c0f9adf8ae
Amine DERRAGUI <aderragu@e2r9p10.42.fr>
2015-12-17 22:48:42 +0100

    fichier header du jeu 17/12

--- fillit.h
+++ fillit.h
@@ -25,16 +25,16 @@ typedef	struct	s_tetri
 	int			p3[2];
 }				t_tetri;
 
-t_tetri	*ft_global_check(char *file_name);
-size_t	ft_strlen(const char *s);
-void	ft_putstr(char const *s);
-long	ft_sqrt(int nb);
-int		ft_bsq(int nb);
-t_tetri	*ft_block_check(char *buf, t_tetri *tetri);
-t_tetri	*ft_pattern_check(char *buf, t_tetri *tetri);
-int 	ft_norme_vector(int x, int y, int x_ref, int y_ref);
-void	ft_p1(char *buf, t_tetri *tetri, int x_ref, int y_ref);
-void	ft_p2(char *buf, t_tetri *tetri, int x_ref, int y_ref);
-void	ft_p3(char *buf, t_tetri *tetri, int x_ref, int y_ref);
+t_tetri			*ft_global_check(char *file_name, int *pcs);
+size_t			ft_strlen(const char *s);
+void			ft_putstr(char const *s);
+long			ft_sqrt(int nb);
+int				ft_bsq(int nb);
+t_tetri			*ft_block_check(char *buf, t_tetri *tetri);
+t_tetri			*ft_pattern_check(char *buf, t_tetri *tetri);
+int				ft_norme_vector(int x, char *buf);
+int				ft_p1(char *buf, t_tetri *tetri, int x_ref, int y_ref);
+int				ft_p2(char *buf, t_tetri *tetri, int x_ref, int y_ref);
+int				ft_p3(char *buf, t_tetri *tetri, int x_ref, int y_ref);
 
 #endif
