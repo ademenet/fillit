00e4f96e75245924038a9571bba334e46a563d4f
Alain DEMENET <ademenet@e2r6p9.42.fr>
2015-12-22 12:02:49 +0100

    Compile le fillit, bug : segfault a resoudre.

--- main_true.c
+++ main_true.c
@@ -1,16 +1,36 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_true.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/22 11:50:45 by ademenet          #+#    #+#             */
+/*   Updated: 2015/12/22 11:59:54 by ademenet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "./inc/fillit.h"
 
 int	main(int argc, char **argv)
 {
 	t_tetri	*tetris;
-	int	pcs;
+	int		pcs;
+	int		bsq;
+	char	**map;
 
 	pcs = 0;
-	tetris = malloc(sizeif(t_tetri) * 26);
+	tetris = malloc(sizeof(t_tetri) * 26);
 	tetris = ft_global_check(argv[1], &pcs);
-	if (tetris == NULL || !pcs || pcs > 26)
-		printf("error\n");
-
+	//if (tetris == NULL || !pcs || pcs > 26)
+	//	printf("error\n"); //ATTENTION printf
+	bsq = ft_bsq(pcs);
+	map = ft_createmap(bsq);
+	backtracking(tetris, map, bsq, 0, 0);
+	while (map--)
+	{
+		printf("%s", *map);
+	}
+	argc--;
+	return (0);
 }
