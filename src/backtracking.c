bbeb280691f0a26cc46b772574226b567324d7d7
Alain DEMENET <ademenet@e2r7p14.42.fr>
2015-12-22 14:55:14 +0100

    Ajout de conditions pour gerer le passage d'une ligne a l'autre.

--- src/backtracking.c
+++ src/backtracking.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   backtracking.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ademenet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/11 10:37:18 by ademenet          #+#    #+#             */
-/*   Updated: 2015/12/21 16:32:39 by gvillat          ###   ########.fr       */
+/*   Updated: 2015/12/22 14:44:18 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,16 @@ int		backtracking(t_tetri *tetris, char **map, int bsq, int y, int x)
 	if (ft_check(p0, bsq, map, tetris[i]))
 	{
 		ft_printmap(map, p0, tetris[i], i);
-		backtracking(&tetris[i + 1], map, bsq, y, x + 1);
+		if (x == bsq - 1)
+		{
+			printf("First\n");
+			backtracking(&tetris[i + 1], map, bsq, y + 1, 0);
+		}
+		else
+		{
+			printf("Second\n");
+			backtracking(&tetris[i + 1], map, bsq, y, x + 1);
+		}
 	}
 	return (0);
 }
