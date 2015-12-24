e188bb8b1dc7b15be3dfb1712c00496068ba13f0
Alain DEMENET <ademenet@e2r7p14.42.fr>
2015-12-24 10:22:15 +0100

    Update

--- src/ft_printmap.c
+++ src/ft_printmap.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printmap.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ademenet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/18 14:40:40 by ademenet          #+#    #+#             */
-/*   Updated: 2015/12/21 16:15:33 by gvillat          ###   ########.fr       */
+/*   Updated: 2015/12/23 16:10:05 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fillit.h"
 
-void	ft_printmap(char **map, int p0[2], t_tetri tetris, int i)
+void	ft_printmap(char **map, int pos, t_tetri tetris, int i)
 {
 	map[p0[0]][p0[1]] = i + 'A';
 	map[p0[0] + tetris.p1[0]][p0[1] + tetris.p1[1]] = i + 'A';
