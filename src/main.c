804913bce2aca11fe24f484bfc404105ef2f5947
tvisenti <tho.visentin@gmail.com>
2015-12-27 09:50:31 +0100

    Norme ok, le programme fonctionne

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aderragu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/11 10:38:09 by aderragu          #+#    #+#             */
-/*   Updated: 2015/12/26 11:28:09 by tvisenti         ###   ########.fr       */
+/*   Updated: 2015/12/27 09:48:50 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,17 @@ int			main(int argc, char **argv)
 	t_tetri	*tetris;
 	int		pcs;
 	int		cnt;
-	//int bonus;
 
 	pcs = 0;
 	cnt = -1;
-	//bonus = argc > 2 && argv[2][0] == 'b' ? 1 : 0;
 	tetris = ft_global_check(argv[1], &pcs);
 	if (tetris == NULL || !pcs || pcs > 26)
 		write(1, "error\n", 6);
 	else
 	{
-		ft_putstr("Nombre de tetriminos : ");
-		ft_putnbr(pcs);
-		ft_putstr(RESET);
 		ft_letter_assignation(tetris);
 		ft_init(tetris, pcs);
 	}
 	argc--;
 	return (0);
 }
-/*printf(" . = %d, # = %d, n = %d\n",dot_cnt, shrp_cnt, nwl_cnt);
-gcc -Wall -Werror -Wextra *.c ./test/main_test.c
-./a.out ./test/file_test*/
